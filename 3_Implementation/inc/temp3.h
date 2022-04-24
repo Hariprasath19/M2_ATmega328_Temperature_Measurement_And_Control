e14f7e1cd500828580ec9c3eeb6c3b1cf39e2258
HARIPRASATH S <101514904+Hariprasath19@users.noreply.github.com>
2022-04-24 22:43:29 +0530

    Update temp3.h

--- 3_Implementation/inc/temp3.h
+++ 3_Implementation/inc/temp3.h
@@ -1,5 +1,5 @@
-#ifndef __T_3_H__
-#define __T_3_H__
+#ifndef __TEMP_3_H__
+#define __TEMP_3_H__
 #define PWM_20_PERCENT (205) /**< Output PWM for 20% duty cycle */
 #define PWM_40_PERCENT (410) /**< Output PWM for 40% duty cycle */
 #define PWM_70_PERCENT (717) /**< Output PWM for 70% duty cycle */
@@ -24,8 +24,8 @@ void InitTimer();
 /**
  * @brief Produce duty cycle in PWM according to i/p ADC value
  * 
- * @param[in] temp The ADC value from t2
+ * @param[in] temp The ADC value from temp2
  */
 void activity3_PWM(uint16_t temp);
 
-#endif /* __T_3_H__ */
+#endif /* __TEMP_3_H__ */
